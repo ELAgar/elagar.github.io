bf6667d8ccb5131163fa52ab14937bade24df3c9
Elvin Agaragimow <imejibuh@gmail.com>
2018-02-11 13:12:28 +0200

    change to slider

---
+++ js/js.js
@@ -0,0 +1,26 @@
+$(document).ready( function () {
+
+	$('.sliderMini ul').lightSlider({
+        item: 3,
+        loop: false,
+        slideMove: 3,
+        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+        speed: 600,
+        responsive: [
+            {
+                breakpoint: 960,
+                settings: {
+                    item: 2,
+                    slideMargin: 6
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    item: 1
+                }
+            }
+        ]
+	});
+
+});
